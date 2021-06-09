77c3d0e1798f3280f4beb261588d283074411185
vikashhack1 <74226641+vikashhack1@users.noreply.github.com>
2021-06-09 17:57:44 +0530

    Add files via upload

---
+++ ScriptToGetCerts.ps1
@@ -0,0 +1,12 @@
+﻿
+$OutputFolder = 'D:\AzureAutomation\Certs\ExportedCerts'
+$CertificateNames = Get-Content D:\AzureAutomation\Certs\CertsTobeUploaded.txt
+
+foreach ($certificate in $CertificateNames) {
+    Write-Verbose $certificate
+
+    $localCert = Export-OaaSKeyVaultCertificate -Name $certificate -OutputFolder $OutputFolder
+    Write-Verbose $localCert
+
+
+}
